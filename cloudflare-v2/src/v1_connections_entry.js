53fb8f8bc2010ecabc4c96850d78735f1dc6a0f6
Mkay <264876915+dudemkay@users.noreply.github.com>
2026-09-16 17:59:44 +0100

    feat: surface logical route editor in portal

--- cloudflare-v2/src/v1_connections_entry.js
+++ cloudflare-v2/src/v1_connections_entry.js
@@ -5,6 +5,7 @@ import { withCTraderCbotConnections } from './dashboard_ctrader_cbot_connections
 import { withMt5ConnectorConnections } from './dashboard_mt5_connector_connections.js';
 import { withTelegramBotSource } from './dashboard_telegram_bot_source.js';
 import { withGranularRoutingConsole } from './dashboard_granular_routing.js';
+import { withLogicalRouteEditor } from './dashboard_logical_route_editor.js';
 import { handleV1AdminConnectionsRequest } from './http/v1_admin_connections_account_controls.js';
 import { handleV1AdminCTraderCbotRequest } from './http/v1_admin_ctrader_cbot.js';
 import { handleV1AdminMt5ConnectorRequest } from './http/v1_admin_mt5_connector.js';
@@ -31,7 +32,8 @@ async function enhancePortalResponse(response) {
   const html = await response.text();
   const headers = new Headers(response.headers);
   headers.set('Cache-Control', 'no-store');
-  return new Response(withGranularRoutingConsole(withTelegramBotSource(withMt5ConnectorConnections(withCTraderCbotConnections(withSimplifiedAccountControls(withUnifiedTradingConnections(html)))))), {
+  const enhanced = withGranularRoutingConsole(withTelegramBotSource(withMt5ConnectorConnections(withCTraderCbotConnections(withSimplifiedAccountControls(withUnifiedTradingConnections(html))))));
+  return new Response(withLogicalRouteEditor(enhanced), {
     status: response.status,
     statusText: response.statusText,
     headers,
